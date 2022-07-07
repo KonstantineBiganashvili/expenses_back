088da563db2efec0e7b21a35c4cac37023afb4fa
kbiganashvili <k.biganashvili.websmart@gmail.com>
2022-07-07 16:15:40 +0400

    added error handling

--- app/controller/expense.controller.js
+++ app/controller/expense.controller.js
@@ -1,6 +1,10 @@
 const Expense = require('../model/Expense');
 
 module.exports.getExpense = async (req, res) => {
-    const expenses = await Expense.findAll();
-    res.send(expenses);
+    try {
+        const expenses = await Expense.findAll();
+        res.send(expenses);
+    } catch (error) {
+        res.status(422).send({ errMsg: error });
+    }
 };
