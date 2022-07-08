461aed883ab9208023731fdcc52309d1f70c6c5c
kbiganashvili <k.biganashvili.websmart@gmail.com>
2022-07-08 14:11:13 +0400

    feautre: update/patch

--- app/controller/expense.controller.js
+++ app/controller/expense.controller.js
@@ -36,3 +36,36 @@ module.exports.createExpense = async (req, res) => {
         return res.status(422).send({ error });
     }
 };
+
+module.exports.editExpense = async (req, res) => {
+    const { name, cost } = req.body;
+    const { id } = req.params;
+    const errorsArray = [];
+
+    const expense = await Expense.findByPk(id);
+    if (!expense)
+        return res.status(404).send({ message: 'Expense does not exist!' });
+
+    if (!name && !cost) {
+        errorsArray.push('You must change at least one field!');
+    } else {
+        if (name) {
+            if (!name.trim()) errorsArray.push('Name must not be empty!');
+        }
+
+        if (cost) {
+            if (typeof cost !== 'number')
+                errorsArray.push('Cost must be a number!');
+        }
+    }
+
+    if (errorsArray.length)
+        return res.status(422).send({ message: errorsArray });
+
+    try {
+        await Expense.update({ name, cost }, { where: { id } });
+        return await this.getExpense(req, res);
+    } catch (error) {
+        return res.status(422).send({ answer: error });
+    }
+};
